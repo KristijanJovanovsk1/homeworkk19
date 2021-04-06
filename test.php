3df1aa8909003810dd7d8e745370e645de8cb454
KristijanJovanovsk1 <kristijanjovanovski33@yahoo.com>
2021-04-06 16:44:29 +0200

    Initial commit

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+echo "Hello WORLD";
+
+$one = 10;
+$two = 20;
+$three = 30;
+$four = 40;
+ 
+
+
+
+echo $two-$one;
+echo $two+$one;
+echo $three*$four;
+echo $four / $one;
+echo $four / $one;
+
+
+
+echo$result=$two-$one;
+echo$result=$two+$one;
+echo$result=$three*$four;
+echo$result=$four / $one;
+echo$result=$four / $one;
+
+
+
